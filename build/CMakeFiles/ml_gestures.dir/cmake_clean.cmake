7c592c51c5d1eb7870e9a0f8a604523893b35b1c
yassersan <yasser.sanobar@gmail.com>
2025-10-30 10:44:11 -0700

    message: Initial commit of build artifacts

---
+++ build/CMakeFiles/ml_gestures.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ml_gestures.dir/src/main.cpp.o"
+  "CMakeFiles/ml_gestures.dir/src/main.cpp.o.d"
+  "ml_gestures"
+  "ml_gestures.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ml_gestures.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
